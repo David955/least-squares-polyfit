538ddb9a4dd9536b75a00bb56ceefdfd04621f8d
David Polakovic <37373827+David955@users.noreply.github.com>
2020-03-19 15:26:07 +0100

    created aproximatin parametres input and loading from file

---
+++ input_check.m
@@ -0,0 +1,13 @@
+%% invalid input handling
+
+       % degree of polynomial can not be bigger than # of points - 1
+if     k > N - 1
+        fprintf(['error: For a set of N points, the maximum order\n' ...
+                  'of the polynomial is k = N - 1'])
+        wrong_input = 1;
+    
+       % polynomial has to have at least one degree 
+elseif k <= 0
+        fprintf('error: the polynomial degree can not be < 1');
+        wrong_input = 1;
+end
